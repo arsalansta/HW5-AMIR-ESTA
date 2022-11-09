eedf469f45cb810a07baea98155ea03ebfab7436
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-09 12:07:03 -0600

    Update header.php

--- header.php
+++ header.php
@@ -1,45 +1,30 @@
-
-		
-		 
-		 
-		 
- <meta charset="utf-8">
- <meta name="viewport" content="width=device-width, initial-scale=1">
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
- rel="stylesheet"
- integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6
- +fzT" crossorigin="anonymous">
- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
- 
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
-		
-  <a class="navbar-brand" href="http://hw5-mis4013-amir.amiresta.oucreate.com/">Homework 5 HomePage</a>
- 
-   
-     
-   
-    
-            <a class="navbar-brand" href="http://hw4-mis4013-amir.amiresta.oucreate.com/">Homework 4 HomePage</a>
-	  
-	  
-		<a class ="navbar-brand" href="multiply.html"> multiply</a>
-  
-      
-      
-   
-</nav> 	 
-		 
-		 
-		
-  <style>
-.center {
-  margin: auto;
-  width: 60%;
-  border: 3px solid #000000;
-  padding: 10px;
-}
-	  h1 {text-align: center;}
-p {text-align: center;}
-div {text-align: center;}
-</style>
-  
+<nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="function1.html">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="function2.html">Features</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="function3.html">Pricing</a>
+        </li>
+	       <li class="nav-item">
+          <a class="nav-link" href="function4.html">Pricing</a>
+        </li> 
+	      <li class="nav-item">
+          <a class="nav-link" href="function5.html">Pricing</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link disabled">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
