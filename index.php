80b2a1c7c1921b3ca77ef795304cc5a926f56979
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-09 00:00:07 -0600

    Update index.php

--- index.php
+++ index.php
@@ -29,24 +29,12 @@ Click me to display Date and Time.</button>
   </form>
    
     
+ <script type = "text/JavaScript">
+         function Hello() {
+            alert("Hello, World");
+         }
+      </script>
 
-
-
-      multiplyit(2,4);
-    
-                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                             
-
-      
-      function multiplyit (number1, number2) {
-  return p1 * p2;
-}
-
-    
-  
-    multiplyit(2,4);
-  
-    
-    
     
        
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
