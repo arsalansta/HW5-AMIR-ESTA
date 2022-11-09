8e4e5557d45054fc3a6fc908ad9c1f2b14d57d38
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-09 00:04:14 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,13 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   
-
+ <base href = "https://www.tutorialspoint.com/" />
+      
+      <script type = "text/JavaScript">
+         function Hello() {
+            alert("Hello, World");
+         }
+      </script>
     
   
   
