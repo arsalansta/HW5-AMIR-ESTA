0440102aabf324dd5d7cb7fcd8f89cc69aa79ca7
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-08 23:29:16 -0600

    Update index.php

--- index.php
+++ index.php
@@ -23,9 +23,12 @@
    
     
     
-    <button type="button"
+<button type="button"
 onclick="document.getElementById('demo').innerHTML = Date()">
 Click me to display Date and Time.</button>
+
+<p id="demo"></p>
+
 
     
     
