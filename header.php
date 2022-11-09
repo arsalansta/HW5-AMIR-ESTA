532c29ad2a7b7b848b6198655724d9b113f83991
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-08 22:17:25 -0600

    Create header.php

---
+++ header.php
@@ -0,0 +1,47 @@
+
+		
+		 
+		 
+		 
+ <meta charset="utf-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+ rel="stylesheet"
+ integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6
+ +fzT" crossorigin="anonymous">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+ 
+	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+		
+  <a class="navbar-brand" href="http://hw4-mis4013-amir.amiresta.oucreate.com/">Homework 4 HomePage</a>
+ 
+   
+     
+   
+    
+            <a class="navbar-brand" href="instructors.php">instructors</a>
+	  
+	  
+    
+    
+      
+  
+      
+      
+   
+</nav> 	 
+		 
+		 
+		
+  <style>
+.center {
+  margin: auto;
+  width: 60%;
+  border: 3px solid #000000;
+  padding: 10px;
+}
+	  h1 {text-align: center;}
+p {text-align: center;}
+div {text-align: center;}
+</style>
+  
