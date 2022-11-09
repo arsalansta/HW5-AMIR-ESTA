0b1c97a2f551e6b6a70d70c0bd2b120cc1d13a89
arsalansta <99049069+arsalansta@users.noreply.github.com>
2022-11-08 23:13:15 -0600

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   </head>
   <body>
-
+    
+    
+<form>
+  
+  number: <input typpe="text1" name ="enter first Number">
+  number: <input typpe="text2" name ="enter secont Number">
+  number: <input typpe="text3" name ="enter third Number">
+  number: <input typpe="text4" name ="enter fourth Number">
+  number: <input typpe="text5" name ="enter fifth Number">
+  
+  </form>
    
     <script>
   public static void print (stirng text){system.out.println(text);}
